172ae90aad5ecbeea7697f8efc6cf65eaac2d592
juliazhu <1340772095@qq.com>
2021-05-17 08:48:44 +0800

    commit note and code

---
+++ java_day01_421/Hello.java
@@ -0,0 +1,9 @@
+//课堂小练习
+//开发一个Hello.java 程序，可以输出“小朱 is studying java!”
+//注意文件有中文时，如何处理
+
+public class Hello{
+	public static void main(String[] args){
+		staticystem.out.println("小朱 is studying java!");
+	}
+}
